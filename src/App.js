e34be88de300361c416e0c6083c5255f2d73410a
CodeXRook <lacekitfly@gmail.com>
2019-03-06 02:30:16 -0500

    HashRouter in fragments

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import {HashRouter, Route } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-     <>
-     <h1>Hello world</h1>
-     </>
+     <HashRouter>
+       <>
+      
+       </>
+     </HashRouter>
     );
   }
 }
