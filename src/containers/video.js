f4d1b3119f7da7e885772a87e239e67d156f4ae3
CodeXRook <lacekitfly@gmail.com>
2019-03-06 10:53:57 -0500

    video js done, need video player

--- src/containers/video.js
+++ src/containers/video.js
@@ -2,54 +2,60 @@ import React from 'react';
 
 class Video extends React.Component {
 
-    state={
-        name:'',
-        comment:'',
-        commentsList:[]
+    state = {
+        name: '',
+        comment: '',
+        commentsList: []
     }
 
-    handleNameChange = (e) =>{
+    handleNameChange = (e) => {
         this.setState({ name: e.target.value })
     }
 
-    handleCommentChange = (e) =>{
+    handleCommentChange = (e) => {
         this.setState({ comment: e.target.value })
     }
-    handleClick = (e) =>{
+    handleClick = (e) => {
         e.preventDefault();
-        const {name, comment, commentsList } = this.state;
+        const { name, comment, commentsList } = this.state;
         commentsList.unshift({ name: name, comment: comment });
-        this.setState({ name: '', comment: '', commentsList });    
+        this.setState({ name: '', comment: '', commentsList });
     }
 
     render() {
         console.log(this.state);
-    
-        return(
-            <>
-             <VideoPlayer id={this.props.match.params.id} />
-             <form>
-             <div class="form-group">
-             <label for="exampleInputEmail1">Name</label>
-             <input onChange={this.handleNameChange} value={this.state.name} type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Name..." />
-             </div>
-             <div class="form-group">
-             <label for="exampleInputPassword1">Comment</label>
-             <input onChange={this.handleCommentChange} value={this.state.comment} type="text" class="form-control" id="exampleInputPassword1" placeholder="Comment..." />
-            </div>
-            <button onClick={this.handleClick} type="submit" class="btn btn-primary">Submit</button>
-            </form>
-            <div className='mt-5'>
-            {
-             this.state.commentsList.map((e, i) => {
-                 return(
-                     <div>
-                         <h1>{e.name}</h1>
-                     </div>
-                 )
-             }
 
-            }
+        return (
+            <>
+                <VideoPlayer id={this.props.match.params.id} />
+                <form>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Name</label>
+                        <input onChange={this.handleNameChange} value={this.state.name} type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Name..." />
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Comment</label>
+                        <input onChange={this.handleCommentChange} value={this.state.comment} type="text" class="form-control" id="exampleInputPassword1" placeholder="Comment..." />
+                    </div>
+                    <button onClick={this.handleClick} type="submit" class="btn btn-primary">Submit</button>
+                </form>
+                <div className='mt-5'>
+                    {
+                        this.state.commentsList.map((e, i) => {
+                            return (
+                                <div>
+                                    <h1>{e.name}</h1>
+                                    <p>{e.comment}</p>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </>
+        );
+    }
+}
+export default Video;
 
 
 
