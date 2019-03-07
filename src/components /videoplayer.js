067b43caf57cf129226ba373123c965a803af31e
CodeXRook <lacekitfly@gmail.com>
2019-03-07 16:09:12 -0500

    videoplayer done

--- src/components /videoplayer.js
+++ src/components /videoplayer.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
 const Video=({id}) =>{
+
     
+    return(
+        <iframe title='yt-video' type="text/html" width="640" height= "360"
+        src={link} frameBorder="O"></iframe>
+    );
 }
+
+    export default Video;
