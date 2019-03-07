95ce6691c8cffcbe5c03e41801a57e31a2631ffa
CodeXRook <lacekitfly@gmail.com>
2019-03-07 09:02:05 -0500

    buttons added

--- src/containers/home.js
+++ src/containers/home.js
@@ -47,6 +47,14 @@ class Home extends React.Component {
             
             return(
                 <div className='container'>
+                 <div className="input-group mb-3 mt-5">
+                 <input onChange={this.handleSearchText} type="text" className="form-control" placeholder="Search..." aria-label="Search..." aria-describedby="button-addon2" />
+                 <div className="input-group-append">
+                 <button onClick={this.handleSubmit} className="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
+
+                 </div>
+
+                 </div>
                 
             )
         }
