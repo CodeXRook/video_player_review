a65659dfafa39f07ee2b47ebe63be54ddaf2628a
CodeXRook <lacekitfly@gmail.com>
2019-03-07 16:06:39 -0500

    videoplayer need you tube link

--- src/components /videocard.js
+++ src/components /videocard.js
@@ -3,9 +3,12 @@ import {Link} from 'react-router-dom';
 
 const VideoCard =({image, title,id}) =>{
 
-}
-
-return(
+return (
+    
     <div className='col-6'>
-    <Link to=('/video/'+id)><img src={image} alt=" style={{width:'100%',height:'auto'}}/</div>
-)
+    <Link to={'/video/'+id}> <img src={image} alt=" style={{width:'100%', height:'auto'}} /> </Link>
+    <h5>(title)</h5>
+    </div>
+);
+}
+export default VideoCard;
