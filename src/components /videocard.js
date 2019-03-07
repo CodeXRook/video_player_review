9a02e9667c1689cf93f8a380f4f31a886ad4ca90
CodeXRook <lacekitfly@gmail.com>
2019-03-07 16:03:09 -0500

    video has a link plus and id call

--- src/components /videocard.js
+++ src/components /videocard.js
@@ -2,5 +2,10 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const VideoCard =({image, title,id}) =>{
-    
+
 }
+
+return(
+    <div className='col-6'>
+    <Link to=('/video/'+id)><img src={image} alt=" style={{width:'100%',height:'auto'}}/</div>
+)
